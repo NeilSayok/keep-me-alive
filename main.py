807785419c6fa148284ad0868392abdbddf42243
Sayok Majumder <sayokdeymajumder1998@gmail.com>
2025-06-23 09:37:49 +0530

    Added Server URLs

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import requests
 import datetime
 
 def ping_backend():
-    url = "https://keep-me-alive.org/ping" #replace this url with your backend url & endpoint
+    urls = ["https://eventbook.bluelabs.in","https://eventbookbackend-users.onrender.com","https://eventbookbackend-category.onrender.com"]
     
-    try:
-        print(f"{datetime.datetime.now()}: Pinging {url}")
-        response = requests.get(url, timeout=30)
-        print(f"Response: {response.status_code}")
-        
-        if response.status_code == 200:
-            print("Backend is alive")
-        else:
-            print(f"⚠️ Unexpected status code: {response.status_code}")
+    for url in urls:
+        try:
+            print(f"{datetime.datetime.now()}: Pinging {url}")
+            response = requests.get(url, timeout=30)
+            print(f"Response: {response.status_code}")
             
-    except Exception as e:
-        print(f"Error: {e}")
+            if response.status_code == 200:
+                print("Backend is alive")
+            else:
+                print(f"⚠️ Unexpected status code: {response.status_code}")
+                
+        except Exception as e:
+            print(f"Error: {e}")
 
 if __name__ == "__main__":
     ping_backend()  # Just ping once, then exit
